5f52b19ed0ed495ee789c23ce78237c2e53f7efd
Juliana Clemente <juliana.clementelima@gmail.com>
2023-09-05 13:55:31 -0300

    segunda alteracao

--- migration/docker-database-initial.sql
+++ migration/docker-database-initial.sql
@@ -1,3 +1,20 @@
+-- init.sql
+
+-- Criação da tabela
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    email VARCHAR (100) UNIQUE NOT NULL,
+    password VARCHAR (100) NOT NULL
+);
+
+-- Inserção de dados de exemplo
+INSERT INTO users (username, email, password)
+VALUES
+    ('user1', 'user1@example.com', 'password1'),
+    ('user2', 'user2@example.com', 'password2');
+
+
 
 CREATE TABLE notificacaos(
     NotificacaoID serial primary key,
@@ -5,7 +22,7 @@ CREATE TABLE notificacaos(
     Conteudo VARCHAR(255)
 );
 
-INSERT INTO Notificacaos(NotificacaoID, Usuario, Conteudo) VALUES
+INSERT INTO Notificacaos(Usuario, Conteudo) VALUES
 (1, 'sobre terminar esse curso e esse tcc e ganhar no minino 5k inicial depois 23 seria o suficiente'),
 (2, 'sobre tentar ganhar algo nessa vida caralho to na segunda graduação, e minha vida não mudou.');
 
